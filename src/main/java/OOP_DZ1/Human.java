11fe93558c4ba8169dbc19f31632f9191b9772c3
Dmitrii <idi-1975@yandex.ru>
2023-04-17 23:22:52 +0300

    DZ1.1 Start

---
+++ src/main/java/OOP_DZ1/Human.java
@@ -0,0 +1,52 @@
+package OOP_DZ1;
+
+public class Human {
+    public String name;
+    public String surname;
+    public int age;
+    public boolean gender;
+
+    public Human(String name, String surname, int age, boolean gender) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    @Override
+    public String toString() {
+        return "Human{" + name + " " + surname + "age=" + age + gender + "}";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public boolean isGender() {
+        return gender;
+    }
+
+    public void setGender(boolean gender) {
+        this.gender = gender;
+    }
+}
